14dabe67363adb6a2b019522d8fd1f64a2ab713d
igl-myrick <igl.myrick@gmail.com>
2024-10-21 15:15:52 -0700

    add Description get functionality

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -5,11 +5,13 @@ namespace VendorTracker.Models
   public class Order
   {
     public string Title { get; set; }
+    public string Description { get; }
     private static List<Order> _instances = new List<Order> {};
 
-    public Order(string title)
+    public Order(string title, string description)
     {
       Title = title;
+      Description = description;
       _instances.Add(this);
     }
   }
