99549e246dfb1d50e298c652cacfaebd4c5e8678
igl-myrick <igl.myrick@gmail.com>
2024-10-21 15:34:18 -0700

    add Date get functionality

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -7,13 +7,15 @@ namespace VendorTracker.Models
     public string Title { get; set; }
     public string Description { get; set; }
     public string Price { get; set; }
+    public string Date { get; }
     private static List<Order> _instances = new List<Order> {};
 
-    public Order(string title, string description, string price)
+    public Order(string title, string description, string price, string date)
     {
       Title = title;
       Description = description;
       Price = price;
+      Date = date;
       _instances.Add(this);
     }
   }
