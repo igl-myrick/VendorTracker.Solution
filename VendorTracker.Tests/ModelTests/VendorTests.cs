9fd5594d7e9e5725a0ce0230b6a7acecce95afda
igl-myrick <igl.myrick@gmail.com>
2024-10-22 14:01:05 -0700

    add test for Description get functionality

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -11,26 +11,37 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("name");
+      Vendor newVendor = new Vendor("name", "description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
+    [TestMethod]
     public void GetName_ReturnsName_String()
     {
       string name = "name";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "description");
       string result = newVendor.Name;
       Assert.AreEqual(name, result);
     }
 
+    [TestMethod]
     public void SetName_UpdatesName_String()
     {
       string name = "name";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "description");
       string newName = "new name";
       newVendor.Name = newName;
       string result = newVendor.Name;
       Assert.AreEqual(newName, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "description";
+      Vendor newVendor = new Vendor("name", description);
+      string result = newVendor.Description;
+      Assert.AreEqual(description, result);
     }
   }
 }
