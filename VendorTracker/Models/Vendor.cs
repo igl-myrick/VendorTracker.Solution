ca4321fde53d61bb9680e1fc2a68b62548a36b91
igl-myrick <igl.myrick@gmail.com>
2024-10-22 13:39:50 -0700

    add constructor and instances list

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace VendorTracker.Models
 {
   public class Vendor
   {
     public string Name { get; set; }
+    public List<Vendor> _instances = new List<Vendor> {};
+
+    public Vendor(string name)
+    {
+      Name = name;
+      _instances.Add(this)
+    }
   }
 }
